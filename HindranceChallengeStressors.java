e75efb11abc4ae75c7fadc697056fea828af3c50
MirageM <imiragesz@gmail.com>
2023-02-02 20:06:41 -0500

    Openness, Extraversion

--- HindranceChallengeStressors.java
+++ HindranceChallengeStressors.java
@@ -1,6 +1,22 @@
 //Chapter 1 to Chapter 6
 /* 
 
+Big Five: CANOE (Personality Reflecting Traits)
+
+Conscientiousness:  Dependable, organized, reliable, ambitious, hard-working, preserving
+Agreeableness: Kind, cooperative, sympathetic, helpful, courteous, warm
+Neuroticism: Nervous,
+
+
+
+
+Overall Ability: Cognitive Ability, Emotional Ability, Physical Ability
+Cognitive Ability: Verbal, Quantitative, Reasoning, Spatial, Perceptual
+Emotional Ability: Self-Awareness, Other Awareness, Emotion Regulation, Use of Emotions
+Physical Ability: Strength, Stamina, Flexibility and Coordination, Psychomotor, Sensory
+
+
+
 Hindrance Stressors:
 •	Work
 o	Role conflict
@@ -21,6 +37,10 @@ o	Work responsibility
 o	Family time demands
 o	Personal development
 o	Positive life events
+
+Physiological Strains: illness, high blood pressure, coronary artery disease, headaches, back pain, stomach aches
+Psychological Strains: depression, anxiety, irritability, forgetfulness, inability to think clearly, reduced confidence, burnout
+Behavioral Strains: alcohol and drug use, teeth grinding, compulsive behaviors, overeating
 
 
 
