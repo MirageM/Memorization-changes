ad20d3cae86ba48b46eefc6225c57bb73565303a
MirageM <imiragesz@gmail.com>
2023-02-02 19:40:20 -0500

    hindrance challenge stressors

---
+++ HindranceChallengeStressors.java
@@ -0,0 +1,20 @@
+Hindrance Stressors:
+•	Work
+o	Role conflict
+o	Role ambiguity
+o	Role overload
+o	Daily hassles
+•	Nonwork
+o	Work-family conflict
+o	Negative life events
+o	Financial uncertainty
+
+Challenge Stressors:
+•	Work
+o	Time pressure
+o	Work complexity
+o	Work responsibility
+•	Nonwork
+o	Family time demands
+o	Personal development
+o	Positive life events
