3ed9e0d9038471a6abb3637b1c3cd49fce637f3f
MirageM <imiragesz@gmail.com>
2023-03-11 18:58:46 -0500

    Ability Benevolence Intregrity

--- HindranceChallengeStressors.java
+++ HindranceChallengeStressors.java
@@ -1,4 +1,5 @@
 /*
+ Ability, Benevolence, Integrity
  Moral Awareness -> Moral Judgment -> Moral Intent -> Ethical Behavior
  
  Distributive justice: The perceived fairness of decision-making outcomes
