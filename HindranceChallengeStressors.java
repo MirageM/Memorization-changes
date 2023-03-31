95ac5a3fbe6ff792c2aa45c5780cf811fee43528
MirageM <imiragesz@gmail.com>
2023-03-30 23:18:04 -0400

    Expert Power -> Expertise or Knowledge

--- HindranceChallengeStressors.java
+++ HindranceChallengeStressors.java
@@ -4,9 +4,9 @@ Organizational Power ->
 Legitimate Power: A form of organizational power based on authority or position
 Reward Power: A form of organizational power based on the control of resources or benefits
 Coercive Power: A form of organizational power based on the ability to hand out punishment
-Personal Power
-Expert Power
-Referent Power
+Personal Power ->
+Expert Power: A form of organizational power based on expertise or knowledge
+Referent Power:
 
 
 
