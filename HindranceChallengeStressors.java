f813d211a066c45ecb8ba3ab3d139b981baa15f6
MirageM <imiragesz@gmail.com>
2023-04-11 22:01:49 -0400

    Trust Justice Ethics

--- HindranceChallengeStressors.java
+++ HindranceChallengeStressors.java
@@ -1,4 +1,10 @@
 /*
+Trust: The willingness to be vulnerable to an authority because of positve expectations about the authority's actions and intentions
+Justice: The perceived fairness of an authority's decision making
+Ethics: The degree to which the behaviors of an authority are in accordance with generally accepted moral norms
+
+
+
 Physiological Strains
 Psychological Strains
 Behavioural Strains
